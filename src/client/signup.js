ae75516e4c01b3b4ddaa6e7dab3f40e4b262bee6
Gilson <gilsonsantos1998@gmail.com>
2022-01-24 23:51:13 -0300

    Adddadshadiuhdaijadhij

--- src/client/signup.js
+++ src/client/signup.js
@@ -1,5 +1,31 @@
-function createUser() {
-    var nameValue = document.getElementById("name")
-    var emailValue = document.getElementById("email")
-    var passwordValue = document.getElementById("password")
+document.getElementById("register_button").addEventListener("click", validate)
+
+// var http = new XMLHttpRequest();    
+// http.open('POST', "http://localhost:3333/users", true);
+// http.setRequestHeader('Content-Type', 'application/json',);
+
+async function validate() {
+    // var nameValue = document.getElementById("name").value
+    // var emailValue = document.getElementById("email").value
+    // var passwordValue = document.getElementById("password").value
+
+    const data = {
+        "name": 'teste',
+        "email": "teste@email.com",
+        "password": "teste56"
+    }
+    
+    window.alert("uma parenta de jorge");
+    const rawResponse = await fetch('https://cors-anywhere.herokuapp.com/http://localhost:3333/users', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin':'*'
+        },
+        body: JSON.stringify(data)
+    });
+    const content = await rawResponse.json();
+    
+    window.alert(content); 
 }
